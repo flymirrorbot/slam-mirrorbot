a8cacfb32d210e71b835d59734baa041e83296ed
flymirrorbot <90692795+flymirrorbot@users.noreply.github.com>
2021-09-19 14:05:42 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -45,8 +45,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = button_build.ButtonMaker()
-    buttons.buildbutton("Owner", "https://t.me/flyr3x")
-    buttons.buildbutton("Channel", "https://t.me/flymirror")
+    buttons.buildbutton("Contact", "https://t.me/flyr3x")
+    buttons.buildbutton("Mirror Group", "https://t.me/flymirror")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
